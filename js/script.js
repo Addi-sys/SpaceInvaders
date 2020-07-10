e0a90a723c0f18be2526631775942758bf87b371
Addi <coxonmarc1@gmail.com>
2020-07-10 18:37:48 +0700

    set background, print playership add movements, set parameters, printed alien ships loop, print mothership set movement and laser firing. To do: collisions, scores, images

--- js/script.js
+++ js/script.js
@@ -9,21 +9,48 @@ Here, we create and add our "canvas" to the page.
 We also load all of our images. 
 */
 
+// game speed and difficulty object
+let game = {
+    startspeed: 2,
+    difficulty: 1,
+    listeningToKeyboard: true,
+    level: 1,
+
+};
+
+
+// define lives variable
+let lives = 3;
 
 let canvas;
 let ctx;
 
 canvas = document.createElement("canvas");
 ctx = canvas.getContext("2d");
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 400;
+canvas.height = 440;
 document.body.appendChild(canvas);
 
-let bgReady, heroReady, monsterReady;
-let bgImage, heroImage, monsterImage;
+let bgReady, heroReady, alienOneReady,
+    alienTwoReady,
+    alienThreeReady,
+    alienFourReady,
+    alienFiveReady,
+    mothershipReady,
+    laserPlayerReady,
+    laserAlienReady;
+
+let bgImage, heroImage, alienOne,
+    alienTwo,
+    alienThree,
+    alienFour,
+    alienFive,
+    mothership,
+    laserPlayer,
+    laserAlien;
 
 let startTime = Date.now();
-const SECONDS_PER_ROUND = 30;
+const SECONDS_PER_ROUND = 0;
 let elapsedTime = 0;
 
 function loadImages() {
@@ -38,14 +65,63 @@ function loadImages() {
         // show the hero image
         heroReady = true;
     };
-    heroImage.src = "images/hero.png";
+    heroImage.src = "images/player.png";
 
-    monsterImage = new Image();
-    monsterImage.onload = function() {
+    alienOne = new Image();
+    alienOne.onload = function() {
         // show the monster image
-        monsterReady = true;
+        alienOneReady = true;
     };
-    monsterImage.src = "images/monster.png";
+    alienOne.src = "images/alien1.png";
+
+    alienTwo = new Image();
+    alienTwo.onload = function() {
+        // show the monster image
+        alienTwoReady = true;
+    };
+    alienTwo.src = "images/alien2.png";
+
+    alienThree = new Image();
+    alienThree.onload = function() {
+        // show the monster image
+        alienThreeReady = true;
+    };
+    alienThree.src = "images/alien3.png";
+
+    alienFour = new Image();
+    alienFour.onload = function() {
+        // show the monster image
+        alienFourReady = true;
+    };
+    alienFour.src = "images/alien4.png";
+
+    alienFive = new Image();
+    alienFive.onload = function() {
+        // show the monster image
+        alienFiveReady = true;
+    };
+    alienFive.src = "images/alien5.png";
+
+    mothership = new Image();
+    mothership.onload = function() {
+        // show the monster image
+        mothershipReady = true;
+    };
+    mothership.src = "images/mothership.png";
+
+    laserPlayer = new Image();
+    laserPlayer.onload = function() {
+        // show the monster image
+        laserPlayerReady = true;
+    };
+    laserPlayer.src = "images/playerlaser.png";
+
+    laserAlien = new Image();
+    laserAlien.onload = function() {
+        // show the monster image
+        laserAlienReady = true;
+    };
+    laserAlien.src = "images/alienlaser.png";
 }
 
 /** 
@@ -59,10 +135,34 @@ function loadImages() {
  */
 
 let heroX = canvas.width / 2
-let heroY = 500
+let heroY = 360
 
-let monsterX = 100;
-let monsterY = 100;
+let alienOneX = 40;
+let alienOneY = 100;
+
+let alienTwoX = 40;
+let alienTwoY = 140;
+
+let alienThreeX = 40;
+let alienThreeY = 180;
+
+let alienFourX = 45;
+let alienFourY = 220;
+
+let alienFiveX = 43;
+let alienFiveY = 260;
+
+let mothershipX = 40;
+let mothershipY = 40;
+let mothershipDirectionX = 1;
+
+let laserAlienX = 30;
+let laserAlienY = 30;
+let laserAlienDirectionY = 1;
+
+let laserPlayerX = canvas.width / 2;
+let laserPlayerY = 360;
+let laserPlayerDirectionY = -1;
 
 /** 
  * Keyboard Listeners
@@ -82,6 +182,7 @@ function setupKeyboardListeners() {
     addEventListener("keyup", function(key) {
         delete keysDown[key.keyCode];
     }, false);
+
 }
 
 
@@ -95,13 +196,16 @@ let update = function() {
     // Update the time.
     elapsedTime = Math.floor((Date.now() - startTime) / 1000);
 
+    let mothershipSpeed = game.startspeed * game.difficulty + 2;
+    let laserAlienSpeed = game.startspeed * game.difficulty + 2;
+    let laserPlayerSpeed = game.startspeed * game.difficulty + 2;
 
-    if (38 in keysDown) { // Player is holding up key
-        heroY -= 5;
-    }
-    if (40 in keysDown) { // Player is holding down key
-        heroY += 5;
-    }
+    // if (38 in keysDown) { // Player is holding up key
+    //     heroY -= 5;
+    // }
+    // if (40 in keysDown) { // Player is holding down key
+    //     heroY += 5;
+    // }
     if (37 in keysDown) { // Player is holding left key
         heroX -= 5;
     }
@@ -112,15 +216,15 @@ let update = function() {
     // Check if player and monster collided. Our images
     // are about 32 pixels big.
     if (
-        heroX <= (monsterX + 32) &&
-        monsterX <= (heroX + 32) &&
-        heroY <= (monsterY + 32) &&
-        monsterY <= (heroY + 32)
+        heroX <= (alienOneX + 32) &&
+        alienOneX <= (heroX + 32) &&
+        heroY <= (alienOneY + 32) &&
+        alienOneY <= (heroY + 32)
     ) {
         // Pick a new location for the monster.
         // Note: Change this to place the monster at a new, random location.
-        monsterX = monsterX + 50;
-        monsterY = monsterY + 70;
+        alienOneX = alienOneX + 50;
+        alienOneY = alienOneY + 70;
     }
 
     if (heroX >= canvas.width - 32) {
@@ -129,6 +233,42 @@ let update = function() {
     if (heroX < 0) {
         heroX = 0
     }
+
+    // alien laser movements
+    laserAlienY += laserAlienSpeed * (laserAlienDirectionY * 1.5);
+    // laserAlienX = laserAlienX;
+
+    if (laserAlienY >= canvas.height) {
+        laserAlienY = mothershipY;
+        laserAlienX = mothershipX;
+    }
+
+
+    // player laser movements
+    laserPlayerY += laserPlayerSpeed * (laserPlayerDirectionY * 1.5);
+    // laserAlienX = laserAlienX;
+
+    if (laserPlayerY === 0) {
+        laserPlayerY = heroY;
+        laserPlayerX = heroX + 17;
+    }
+
+    // mothership movements
+    mothershipX += mothershipSpeed * mothershipDirectionX;
+    mothershipY = mothershipY;
+    if (
+        mothershipX >= canvas.width - 32
+    ) {
+        mothershipX = canvas.width - 32
+        mothershipDirectionX = -1;
+    } else if (
+        mothershipX <= 0
+    ) {
+        mothershipX = 0
+        mothershipDirectionX = 1;
+    }
+
+
 };
 
 /**
@@ -141,10 +281,61 @@ var render = function() {
     if (heroReady) {
         ctx.drawImage(heroImage, heroX, heroY);
     }
-    if (monsterReady) {
-        ctx.drawImage(monsterImage, monsterX, monsterY);
+    if (alienOneReady) {
+        let positionChange = 0;
+
+        for (i = 0; i <= canvas.width / 55; i++) {
+            ctx.drawImage(alienOne, alienOneX + positionChange, alienOneY);
+            positionChange += 40
+        }
+
     }
-    ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, 20, 100);
+    if (alienTwoReady) {
+        let positionChange = 0;
+
+        for (i = 0; i < canvas.width / 50; i++) {
+            ctx.drawImage(alienTwo, alienTwoX + positionChange, alienTwoY);
+            positionChange += 40;
+        }
+    }
+    if (alienThreeReady) {
+        let positionChange = 0;
+
+        for (i = 0; i < canvas.width / 50; i++) {
+            ctx.drawImage(alienThree, alienThreeX + positionChange, alienThreeY);
+            positionChange += 40;
+        }
+    }
+    if (alienFourReady) {
+        let positionChange = 0
+
+        for (i = 0; i < canvas.width / 50; i++) {
+            ctx.drawImage(alienFour, alienFourX + positionChange, alienFourY);
+            positionChange += 40
+        }
+    }
+    if (alienFiveReady) {
+        let positionChange = 0
+
+        for (i = 0; i < canvas.width / 50; i++) {
+            ctx.drawImage(alienFive, alienFiveX + positionChange, alienFiveY);
+            positionChange += 40;
+        }
+    }
+    if (mothershipReady) {
+        ctx.drawImage(mothership, mothershipX, mothershipY);
+    }
+
+    if (laserAlienReady) {
+        ctx.drawImage(laserAlien, laserAlienX, laserAlienY);
+    }
+
+    if (laserPlayerReady) {
+        ctx.drawImage(laserPlayer, laserPlayerX, laserPlayerY);
+    }
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(`${SECONDS_PER_ROUND + elapsedTime}`, 20, 20);
+
 };
 
 /**
